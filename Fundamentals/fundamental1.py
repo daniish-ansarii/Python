d3caafb1e02d52fcb2fb4edc9d7fcfc392fb43c2
Md Danish <137596949+daniish-ansarii@users.noreply.github.com>
2026-06-14 22:15:49 +0530

    Implement basic arithmetic and average calculation
    Add basic arithmetic and boolean operations with user input.

---
+++ Fundamentals/fundamental1.py
@@ -0,0 +1,28 @@
+# This is a comment
+a = 10
+a **= 2
+print(a)
+
+a = 10
+a = a + 5
+print(a)
+
+# relational
+var = False
+print(not var)
+print(not (5 > 8))
+
+val = bool(0)
+print(val, type(val))
+
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+sum = float(a) + float(b)
+print("The sum is: ", sum)  
+
+# Write to print the average of three numbers
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+c = input("Enter the third number: ")
+average = (float(a) + float(b) + float(c)) / 3
+print("The average is: ", average)
